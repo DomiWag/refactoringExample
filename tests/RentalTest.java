28eaf7b6c9dd87a3cf3e0f3e4e967c4b35a6d6ed
Dominik Wagner <DomiWag@users.noreply.github.com>
2014-05-14 15:33:05 +0200

    Add JUnit test for Rental

--- tests/RentalTest.java
+++ tests/RentalTest.java
@@ -2,22 +2,26 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class RentalTest extends TestCase {
+
+    private final String MOVIE_TITLE = "Back to the Future";
+    private final int DAYS_RENTED = 90;
+    private Movie movie;
+    private Rental rental;
 
     @Before
     public void setUp() throws Exception {
-
+        movie = new Movie(MOVIE_TITLE, Movie.REGULAR);
+        rental = new Rental(movie, DAYS_RENTED);
     }
 
     @Test
     public void testGetDaysRented() throws Exception {
-
+        assertEquals(DAYS_RENTED, rental.getDaysRented());
     }
 
     @Test
     public void testGetMovie() throws Exception {
-
+        assertEquals(movie, rental.getMovie());
     }
 }
