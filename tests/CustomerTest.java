0cb1aa9c0e28a7666ed621b5e09ae6c5fda7560e
Dominik Wagner <DomiWag@users.noreply.github.com>
2014-05-14 15:38:17 +0200

    Add JUnit test for Customer

--- tests/CustomerTest.java
+++ tests/CustomerTest.java
@@ -2,27 +2,30 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class CustomerTest extends TestCase {
+
+    private final String CUSTOMER_NAME = "John Smith";
+    private final String MOVIE_TITLE = "Back to the Future";
+    private final int RENTAL_DAYS = 90;
+    private Customer customer;
 
     @Before
     public void setUp() throws Exception {
-
+        customer = new Customer(CUSTOMER_NAME);
     }
 
     @Test
     public void testAddRental() throws Exception {
-
+        customer.addRental(new Rental(new Movie(MOVIE_TITLE, Movie.REGULAR), RENTAL_DAYS));
     }
 
     @Test
     public void testGetName() throws Exception {
-
+        assertEquals(CUSTOMER_NAME, customer.getName());
     }
 
     @Test
     public void testStatement() throws Exception {
-
+        assertNotNull(customer.statement());
     }
 }
