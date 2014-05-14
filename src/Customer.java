40e7f98298750304ab6501fd5abc7dd235b5ca9c
Dominik Wagner <DomiWag@users.noreply.github.com>
2014-05-14 17:11:59 +0200

    Refactor Customer: Rename getTotalFrequentFlyerPoints to getTotalFrequentRenterPoints. Add htmlStatement method
    Whoopsie, typo.

--- src/Customer.java
+++ src/Customer.java
@@ -27,11 +27,11 @@ class Customer {
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalFrequentFlyerPoints()) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
 
-    private int getTotalFrequentFlyerPoints() {
+    private int getTotalFrequentRenterPoints() {
         int result = 0;
         Enumeration rentals = this.rentals.elements();
         while (rentals.hasMoreElements()) {
@@ -51,5 +51,18 @@ class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration rentals = this.rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for each rental
+            result += each.getMovie().getTitle()+ ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        //add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+        return result;
+    }
 }
     
