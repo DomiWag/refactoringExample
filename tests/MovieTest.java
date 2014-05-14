2464f27dd027b2d4b7974eaf94bcb02d38414492
Dominik Wagner <DomiWag@users.noreply.github.com>
2014-05-14 14:24:29 +0200

    Add JUnit test for Movie

--- tests/MovieTest.java
+++ tests/MovieTest.java
@@ -2,27 +2,31 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MovieTest extends TestCase {
+
+    private final String MOVIE_NAME = "Back to the Future";
+    private final int PRICE_CODE = 1337;
+    private final int NEW_PRICE_CODE = 31337;
+    private Movie movie;
 
     @Before
     public void setUp() throws Exception {
-
+        movie = new Movie(MOVIE_NAME, PRICE_CODE);
     }
 
     @Test
     public void testGetPriceCode() throws Exception {
-
+        assertEquals(PRICE_CODE, movie.getPriceCode());
     }
 
     @Test
     public void testSetPriceCode() throws Exception {
-
+        movie.setPriceCode(NEW_PRICE_CODE);
+        assertEquals(NEW_PRICE_CODE, movie.getPriceCode());
     }
 
     @Test
     public void testGetTitle() throws Exception {
-
+        assertEquals(MOVIE_NAME, movie.getTitle());
     }
 }
